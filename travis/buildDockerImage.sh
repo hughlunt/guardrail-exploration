0ed9d5e12df55f80e26fa22384a15d3af7c0017c
mmvelasquez-mdsol <mmvelasquez@mdsol.com>
2018-06-14 10:06:15 +0100

    Adding docker login

--- travis/buildDockerImage.sh
+++ travis/buildDockerImage.sh
@@ -5,6 +5,8 @@ sbt dist
 
 docker build -t sitebudgets .
 
+docker login mdsol-payments-docker.jfrog.io
+
 docker tag sitebudgets mdsol-payments-docker.jfrog.io/sitebudgets
 
 docker push mdsol-payments-docker.jfrog.io/sitebudgets
