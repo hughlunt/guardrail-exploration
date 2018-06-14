edaa83a25793429b3d259cf985dcf1f3f8a73823
mmvelasquez-mdsol <mmvelasquez@mdsol.com>
2018-06-14 10:23:20 +0100

    Passing credendtials to docker login

--- travis/buildDockerImage.sh
+++ travis/buildDockerImage.sh
@@ -5,7 +5,7 @@ sbt dist
 
 docker build -t sitebudgets .
 
-docker login mdsol-payments-docker.jfrog.io
+docker login -u="payments-admin" -p="AP8VmfDrdXmMzoBGP4rGDTFUbry" mdsol-payments-docker.jfrog.io
 
 docker tag sitebudgets mdsol-payments-docker.jfrog.io/sitebudgets
 
