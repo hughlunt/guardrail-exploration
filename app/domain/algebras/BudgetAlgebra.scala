e65302fb349a66d8f7c1ba9849718776d80b0c43
Igal Benardete <ibenardetelevis@mdsol.com>
2018-06-20 10:53:02 +0100

    WIP - Create budget item.

--- app/domain/algebras/BudgetAlgebra.scala
+++ app/domain/algebras/BudgetAlgebra.scala
@@ -1,5 +1,8 @@
 package domain.algebras
 
+import domain.entities.Budget
+import scala.language.higherKinds
+
 trait BudgetAlgebra[F[_]] {
 
   def add(budget: Budget): F[Unit]
--- app/domain/entities/Budget.scala
+++ app/domain/entities/Budget.scala
@@ -1,5 +1,19 @@
 package domain.entities
 
-import java.time.ZonedDateTime
+import java.time.Instant
 
-case class Budget(id: BudgetId, ctaId: CtaId, study: StudyId, site: SiteId, payee: PayeeId, validFrom: ZonedDateTime, validUntil: ZonedDateTime)
+case class Budget(id: BudgetId,
+                  ctaId: CtaId,
+                  study: StudyId,
+                  site: SiteId,
+                  validFrom: Instant,
+                  validUntil: Instant,
+                  eventCostCollection: Set[BudgetItem])
+
+
+case class BudgetItem(activityType: String,
+                      id: String,
+                      payee: PayeeId,
+                      validFrom: Instant,
+                      validUntil: Instant,
+                      amount: Double)
