07e88115e093d789eca5936794ccb5ec47db0bf2
krapuru-mdsol <krapuru@mdsol.com>
2018-06-20 17:02:26 +0100

    MCC:398597 - Budget Headers and Items Inyterpreters Impl

--- app/domain/interpreter/BudgetHeaderInterpreter.scala
+++ app/domain/interpreter/BudgetHeaderInterpreter.scala
@@ -3,12 +3,13 @@ package domain.interpreter
 import cats.data.EitherT
 import cats.implicits._
 import domain.algebras.BudgetHeaderAlgebra
-import domain.entities.{BudgetHeader, BudgetId, FEither}
+import domain.entities.{BudgetHeader, BudgetId, FEither, Error}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class BudgetHeaderInterpreter(implicit ec: ExecutionContext) extends BudgetHeaderAlgebra[FEither] {
   override def add(budgetHeader: BudgetHeader): FEither[Unit] = EitherT.right(Future.successful(()))
 
-  override def retrieve(id: BudgetId): FEither[BudgetHeader] = ???
+  override def retrieve(id: BudgetId): FEither[BudgetHeader] = EitherT(Future.successful(Left(Error(" Error "))))
+
 }
