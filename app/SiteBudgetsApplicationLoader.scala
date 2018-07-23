259f18af22caebe05543c1cee1701021feba00ca
Igal Benardete <ibenardetelevis@mdsol.com>
2018-07-23 10:26:34 +0100

    Remove comments.

--- app/SiteBudgetsApplicationLoader.scala
+++ app/SiteBudgetsApplicationLoader.scala
@@ -5,41 +5,19 @@ import router.Routes
 
 class SiteBudgetsApplicationLoader extends ApplicationLoader {
 
-  override def load(context: ApplicationLoader.Context): Application = new SiteBudgetsComponents(context).application
+  override def load(context: ApplicationLoader.Context): Application =
+    new SiteBudgetsComponents(context).application
 
 }
 
 import play.api.ApplicationLoader.Context
 
-class SiteBudgetsComponents(context: Context) extends BuiltInComponentsFromContext(context) {
+class SiteBudgetsComponents(context: Context)
+    extends BuiltInComponentsFromContext(context) {
 
   lazy val homeController: HomeController = HomeController(controllerComponents)
 
-//  override def router = new Routes(httpErrorHandler, homeController)
   lazy val router = new Routes(httpErrorHandler, homeController)
 
   override def httpFilters: Seq[EssentialFilter] = Seq()
 }
-
-/*
-import play.api._
-import play.api.ApplicationLoader.Context
-import play.filters.HttpFiltersComponents
-import router.Routes
-
-class MyApplicationLoader extends ApplicationLoader {
-  def load(context: Context) = {
-    new MyComponents(context).application
-  }
-}
-
-class MyComponents(context: Context)
-  extends BuiltInComponentsFromContext(context)
-    with HttpFiltersComponents
-    with controllers.AssetsComponents {
-  lazy val barRoutes = new bar.Routes(httpErrorHandler)
-  lazy val applicationController = new controllers.Application(controllerComponents)
-
-  lazy val router = new Routes(httpErrorHandler, applicationController, barRoutes, assets)
-}
- */
