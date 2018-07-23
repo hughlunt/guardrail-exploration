238093e59b732051e6ce1c0a0e36e690ea6cc1d5
Igal Benardete <ibenardetelevis@mdsol.com>
2018-07-23 11:43:53 +0100

    Format code.

--- app/SiteBudgetsApplicationLoader.scala
+++ app/SiteBudgetsApplicationLoader.scala
@@ -1,26 +1,25 @@
 import controllers.{HomeController, UtilityController}
 import play.api.mvc.EssentialFilter
 import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
+import play.api.ApplicationLoader.Context
 import router.Routes
 
 class SiteBudgetsApplicationLoader extends ApplicationLoader {
 
   override def load(context: ApplicationLoader.Context): Application =
     new SiteBudgetsComponents(context).application
-
 }
 
-import play.api.ApplicationLoader.Context
-
-class SiteBudgetsComponents(context: Context)
-    extends BuiltInComponentsFromContext(context) {
+class SiteBudgetsComponents(context: Context) extends BuiltInComponentsFromContext(context) {
 
   lazy val homeController: HomeController = HomeController(controllerComponents)
-  lazy val utilityController: UtilityController = UtilityController(
-    controllerComponents)
+  lazy val utilityController: UtilityController = UtilityController(controllerComponents)
 
-  lazy val router =
-    new Routes(httpErrorHandler, homeController, utilityController)
+  lazy val router = new Routes(
+    httpErrorHandler,
+    homeController,
+    utilityController
+  )
 
   override def httpFilters: Seq[EssentialFilter] = Seq()
 }
