bfe37efe9bd42bbb603f48d941da4c601b65d712
Igal Benardete <ibenardetelevis@mdsol.com>
2018-07-30 09:12:27 +0100

    Fix router error.

--- app/SiteBudgetsApplicationLoader.scala
+++ app/SiteBudgetsApplicationLoader.scala
@@ -1,4 +1,4 @@
-import controllers.{HomeController, UtilityController}
+import controllers.{HomeController, SwaggerController, UtilityController}
 import play.api.mvc.EssentialFilter
 import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
 import play.api.ApplicationLoader.Context
@@ -12,12 +12,13 @@ class SiteBudgetsApplicationLoader extends ApplicationLoader {
 
 class SiteBudgetsComponents(context: Context) extends BuiltInComponentsFromContext(context) {
 
-  lazy val homeController: HomeController = HomeController(controllerComponents)
+//  lazy val homeController: HomeController = HomeController(controllerComponents)
+  lazy val swaggerController: SwaggerController = SwaggerController(controllerComponents)
   lazy val utilityController: UtilityController = UtilityController(controllerComponents)
 
   lazy val router = new Routes(
     httpErrorHandler,
-    homeController,
+    swaggerController,
     utilityController
   )
 
