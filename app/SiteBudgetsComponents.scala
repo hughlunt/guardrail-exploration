7133db169478a0d522db0b6c10dbc742b663e535
Izzet Igal Benardete Levis <ibenardetelevis@mdsol.com>
2018-08-16 22:23:33 +0100

    Fix errors.

--- app/SiteBudgetsComponents.scala
+++ app/SiteBudgetsComponents.scala
@@ -4,15 +4,18 @@ import play.api.BuiltInComponentsFromContext
 import play.api.mvc.EssentialFilter
 import router.Routes
 
-class SiteBudgetsComponents(context: Context) extends BuiltInComponentsFromContext(context) {
+class SiteBudgetsComponents(context: Context)
+    extends BuiltInComponentsFromContext(context) {
 
-  lazy val swaggerController: SwaggerController = SwaggerController(controllerComponents)
-  lazy val utilityController: UtilityController = UtilityController(controllerComponents)
+  lazy val swaggerController: SwaggerController = SwaggerController(
+    controllerComponents)
+  lazy val utilityController: UtilityController = UtilityController(
+    controllerComponents)
 
   lazy val router = new Routes(
     httpErrorHandler,
-    utilityController,
-    swaggerController
+    swaggerController,
+    utilityController
   )
 
   override def httpFilters: Seq[EssentialFilter] = Seq()
